cda4a490b50096f40e960109ff3ef1290c67e63e
demandkm <32107352+demandkm@users.noreply.github.com>
2017-10-22 10:02:21 -0500

    Add files via upload

---
+++ NewPaymentTypes.java
@@ -0,0 +1,13 @@
+package com.example.grumwoldemichael.honestour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewPaymentTypes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_payment_types);
+    }
+}
